060e02612416f47c1e04f5289210121477345a59
hamidgokce <hamidgokce@gmail.com>
2022-10-01 01:22:25 +0300

    102 answers

--- Adanzyedocker_container102_answer.sh
+++ Adanzyedocker_container102_answer.sh
@@ -15,54 +15,53 @@ dağıtacak ve gateway olarak da 10.10.10.10 tanımlanacak bir kullanıcı tanı
 network yaratalım. (bu sizin yerel ağınızda kullandığınız bir ağ aralığıysa başka bir 
 cidr kullanabilirsiniz). Bu ağın özelliklerini inspect komutuyla kontrol edin. 
 
-docker network create --driver=bridge --subnet=10.10.0.0/16 --ip-range=10.10.10.0/24 --gateway=10.10.10.10 alistirma-agi
-docker network inspect ...
+  docker network create --driver=bridge --subnet=10.10.0.0/16 --ip-range=10.10.10.0/24 --gateway=10.10.10.10 alistirma-agi # driver girmesek de default olarak bridge sececekti
+  docker network inspect alistirma-agi 
 
 3: nginx imajının 1.16 versiyonundan web1 adından detached bir container yaratın. 
 Bu containerı default bridge networküne değil de alistirma-agi networküne bağlı 
-olarak yaratın. Host'un 8080 tcp portuna gelen isteklerin bu containerın 80 portuna 
+olarak yaratın. Hostun 8080 tcp portuna gelen isteklerin bu containerın 80 portuna 
 gitmesini sağlayın.
 
-docker container run -d --name web1 --net alistirma-agi -p 8080:80 nginx:1.16
-docker ps 
-
+  docker container run -d --name web1 --net alistirma-agi -p 8080:80 nginx:1.16 
+  docker ps 
 
 4: Bu sisteme browser üzerinden erişin ve daha sonra bir kaç kez sayfayı refresh edin. 
-Ardından bu container'ın loglarına erişerek az önceki erişimlerinizin loglarını kontrol edin. 
+Ardından bu containerın loglarına erişerek az önceki erişimlerinizin loglarını kontrol edin. 
 
-browserdan 127.0.0.1:8080 ni acalim 
-docker logs ...
+  browserdan 127.0.0.1:8080 ni acalim 
+  docker logs web1 # or id
 
-5: Container loglarını follow modunda takip ederken browser'dan bu web sitesinde olmayan 
-bir url'e gitmeye çalışın. Örneğin xyz.html Bunun ürettiği hatayı canlı olarak loglardan
+5: Container loglarını follow modunda takip ederken browserdan bu web sitesinde olmayan 
+bir urle gitmeye çalışın. Örneğin xyz.html Bunun ürettiği hatayı canlı olarak loglardan
 takip edin. 
 
-docker ps
-docker logs ... -f (-f canli gormemizi saglamaktadir)
-sayfada yenileme yaptigimizda veya olmayan bir url ye gittigimizde hatayi gorecegiz
+  docker ps
+  docker logs -f web1 # -f canli gormemizi saglamaktadir
+  sayfada yenileme yaptigimizda veya olmayan bir url ye gittigimizde hatayi gorecegiz
 
 6: ozgurozturknet/adanzyedocker imajından test1 adından bir container yaratın. 
--dit ile yaratın sh shellini açın. Bu container default bridge network'e bağlı olsun. 
+-dit ile yaratın sh shellini açın. Bu container default bridge networke bağlı olsun. 
 
-docker container run -dit --name test1 ozgurozturknet/adanzyedocker sh
-docker ps 
+  docker container run -dit --name test1 ozgurozturknet/adanzyedocker sh
+  docker ps 
 
-7: Bu container'ı "alistirma-agi" networküne de bağlayın.
+7: Bu containerı "alistirma-agi" networküne de bağlayın.
 
-docker network connect alistirma-agi test1
+  docker network connect alistirma-agi test1
 
-8: Container'a attach işlemiyle bağlanın ve container içerisinden web1'i pinglemeye çalışın. 
-Container'ı kapamadan çıkın. 
+8: Containera attach işlemiyle bağlanın ve container içerisinden web1i pinglemeye çalışın. 
+Containerı kapamadan çıkın. 
 
-docker attach test1
-ping web1
-ctrl + p + q
-docker ps 
+  docker attach test1
+  ping web1 # ayni baga baglandigi icin pingleyebilmekteyiz. birbirlerinin DNS olarak isimlerini cozumleyebiliyorlardi
+  ctrl + p + q
+  docker ps 
 
 9: Bu containerların çalıştığını kontrol edin ve ardından çalışıyor haldeyken bunları silin. 
 
-docker container rm -f  test1 web1
-docker ps
+  docker container rm -f  test1 web1
+  docker ps
 
 10: Terminalde eğitim klasörünün altındaki kisim4/bolum43 klasörüne geçin.
 
@@ -71,23 +70,23 @@ docker ps
 80 portunu host üstündeki 80 portuyla publish edin. env.list dosyasının bu containera
  enviroment variable olarak aktarılmasını sağlayın. 
 
- docker container run -d --name websrv --net alistirma-agi -p 80:80 --cpus="2" --env-file ./env.list ozgurozturknet/webkayit
+  docker container run -d --name websrv --net alistirma-agi -p 80:80 --cpus="2" --env-file ./env.list ozgurozturknet/webkayit
 
 12: ozgurozturknet/webdb imajından mysqldb adinda detached bir container yaratın.
  "alistirma-agi" networküne bağlı olsun. Maksimum 1gb memory kullanacak şekilde kısıtlansın.
   envmysql.list dosyasının bu containera enviroment variable olarak aktarılmasını sağlayın. 
 
-docker container run -d --name mysqldb --env-file .\envmysql.list --net alistirma-agi --memory=1g ozgurozturknet/webdb
+  docker container run -d --name mysqldb --env-file .\envmysql.list --net alistirma-agi --memory=1g ozgurozturknet/webdb
 
 13: mysqldb containerının loglarını kontrol ederek düzgün şekilde başlatılabildiğini teyit
  edin. 
 
-docker logs mysqldb
-"mysqld: ready for connections."
+  docker logs mysqldb
+  "mysqld: ready for connections."
 
-14: Browser'dan websrv container'ının yayınladığı web sitesine bağlanın. 
+14: Browserdan websrv containerının yayınladığı web sitesine bağlanın. 
 Karşınıza çıkan formu doldurup bir tane jpg dosyayı da seçerek add tuşuna basın. Ardından kayitlari gör diyerek işlemin başarılı olduğunu teyit edin. 
 
 127.0.0.1 browser a gir bilgileri ve jpg dosyasini ekle
 
-15: Oluşturduğunuz containerları ve alistirma-agi'ni silin.
+15: Oluşturduğunuz containerları ve alistirma-agini silin.
