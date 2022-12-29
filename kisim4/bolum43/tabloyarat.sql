c750819cf33d3b8aaf3821cd6c5e89d544079311
hamidgokce <hamidgokce@gmail.com>
2022-12-29 02:08:16 +0200

    source

--- kisim4/bolum43/tabloyarat.sql
+++ kisim4/bolum43/tabloyarat.sql
@@ -1,2 +1,2 @@
 USE proje;
-CREATE TABLE visitors (name VARCHAR(30), email VARCHAR(30), phone VARCHAR(30), photo VARCHAR(30));
+CREATE TABLE visitors (name VARCHAR(30), email VARCHAR(30), phone VARCHAR(30), photo VARCHAR(30));   
